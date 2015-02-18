8acb77fcf40c9ea8b520faa06e0060e1be4d605b
bredele <olivier.wietrich@gmail.com>
2015-02-17 19:23:03 -0700

    add clear handler

--- index.js
+++ index.js
@@ -11,14 +11,20 @@ module.exports = function(canvas) {
 function Scene(canvas) {
   this.el = canvas || document.createElement('canvas');
   this.gl = init(this.el);
-  this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+  this.clear();
 }
 
 
 Scene.prototype.add = function(obj) {
   obj.context(this.gl);
+  this.clear();
   obj.draw();
+};
+
+
+Scene.prototype.clear = function() {
+  this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  this.gl.clear(gl.COLOR_BUFFER_BIT);
 };
 
 
